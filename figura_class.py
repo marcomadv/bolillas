678de279238cab96841d6641fb610c70b9df749d
marcomadv <imarcoma@gmail.com>
2023-03-28 15:13:22 +0200

    crear bolillas

--- figura_class.py
+++ figura_class.py
@@ -1,15 +1,11 @@
 class Rectangulo():
-    def __init__(self,pos_x, pos_y, color=(255, 255, 255), w = 20, h = 20): #metodo constructor o inicializador
+    def __init__(self,pos_x, pos_y, color=(255, 255, 255), w = 20, h = 20, vx = 1, vy = 1): #metodo constructor o inicializador
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.color = color
         self.w = w
         self.h = h
 
-        self.vx = 0
-        self.vy = 0
-
-    def velocidad(self, vx, vy):
         self.vx = vx
         self.vy = vy
     
@@ -20,5 +16,23 @@ class Rectangulo():
             self.vx *= -1
         if self.pos_y >= ymax or self.pos_y <= 0: #los limites de y
             self.vy *= -1
+
+class Bolillas():
+    def __init__(self, pos_x, pos_y, color = (245, 74, 107), radio = 5, vx = 1, vy = 1):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.color = color 
+        self.radio = radio
+        self.vx = vx
+        self.vy = vy 
+    
+    def mover(self, xmax, ymax):
+        self.pos_x += self.vx
+        self.pos_y += self.vy
+        if self.pos_x >= xmax or self.pos_x <=0: #los limites en x
+            self.vx *= -1
+        if self.pos_y >= ymax or self.pos_y <= 0: #los limites de y
+            self.vy *= -1
+
 
 
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame as pg
-from figura_class import Rectangulo
+from figura_class import Rectangulo, Bolillas
 
 #inicializar todos los modulos de pygame: pantallas, objetos, sonidos, teclados, etc.
 pg.init()
@@ -10,22 +10,12 @@ pg.display.set_caption( "Bolillas" ) #agregar un titulo a mi ventana
 
 game_over = False
 
-rectangulo1 = Rectangulo(400, 300) #rect blanco
-rectangulo2 = Rectangulo(300, 200, (192, 6, 211), 30, 30) #rect violeta
-rectangulo1.velocidad(1, 1)
-rectangulo2.velocidad(2, 2)
+#rectangulo1 = Rectangulo(400, 300) #rect blanco
+#rectangulo2 = Rectangulo(300, 200, (192, 6, 211), 30, 30) #rect violeta
 
-'''
-x = 0
-vx = 1
-y = 300
-vy = 1
+circulo1 = Bolillas(400, 300)
+circulo2 = Bolillas(300, 200, (192, 6, 211), radio = 30)
 
-x2 = 80
-vx2 = 1
-y2 = 30
-vy2 = 1
-'''
 while not game_over:
 
     for eventos in pg.event.get(): #capturar todos los eventos mientras el bucle se ejecute
@@ -35,30 +25,17 @@ while not game_over:
     
     pantalla.fill((33, 14, 119))#asignar un color a la pantalla
 
-    rectangulo1.mover(800, 600)
-    rectangulo2.mover(800, 600)
-    '''
-    y += vy
-    x += vx
-    if x >= 800 or x == 0: #los limites en X
-        vx *= -1
-    if y >= 600 or y == 0: #los limites de y
-        vy *= -1
+    #rectangulo1.mover(800, 600)
+    #rectangulo2.mover(800, 600)
+    circulo1.mover(800, 600)
+    circulo2.mover(800, 600)
 
-    y2 += vy2
-    x2 += vx2
-    if x2 >= 800 or x2 == 0: #los limites en X
-        vx2 *= -1
-    if y2 >= 600 or y2 == 0: #los limites de y
-        vy2 *= -1
-    '''
-  
     #la pantalla o surface, color en tupla de rgb, posiciones(posicionAncho,posicionLargo,tamaño del rec. largo, tamaño del rec. ancho)
-    pg.draw.rect(pantalla, rectangulo1.color, (rectangulo1.pos_x, rectangulo1.pos_y, rectangulo1.w, rectangulo1.h))#dibujar un rectangulo 
-    pg.draw.rect(pantalla, rectangulo2.color, (rectangulo2.pos_x, rectangulo2.pos_y, rectangulo2.w, rectangulo2.h))#dibujar un rectangulo 
-
-
-
+    #pg.draw.rect(pantalla, rectangulo1.color, (rectangulo1.pos_x, rectangulo1.pos_y, rectangulo1.w, rectangulo1.h))#dibujar un rectangulo 
+    #pg.draw.rect(pantalla, rectangulo2.color, (rectangulo2.pos_x, rectangulo2.pos_y, rectangulo2.w, rectangulo2.h))#dibujar un rectangulo 
+    pg.draw.circle(pantalla,circulo1.color, (circulo1.pos_x, circulo1.pos_y), circulo1.radio)
+    pg.draw.circle(pantalla,circulo2.color, (circulo2.pos_x, circulo2.pos_y), circulo2.radio)
+    
     pg.display.flip()#funcion para cargar toda la configuracion que va dentro de la pantalla
 
 pg.quit()
